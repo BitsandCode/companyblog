cb955ad75012a78d755b0e4923f6a00b8ddbbdc6
BenKalungi <glukooya@gmail.com>
2019-08-19 14:27:59 +0300

    added posts reducer to the global reduce

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer/postsReducer'
 
 //import reducers...
 
 export default combineReducers({
-
+       postsData: postsReducer
 })
