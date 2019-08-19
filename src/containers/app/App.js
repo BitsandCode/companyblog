092b2ef5f7f182ab95cadb1c5f48d0787e96b837
BenKalungi <glukooya@gmail.com>
2019-08-19 14:26:12 +0300

    imported routes, and browser-router to build app navigation

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+import Routes from '../../routes'
 import '../../styles.scss';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+   <Router>
+     <Routes/>
+   </Router>
   );
 }
 
